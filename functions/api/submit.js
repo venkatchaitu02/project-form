7f343a06c78efea2347270364e126935648edfb6
venkatchaitu02 <113192819+venkatchaitu02@users.noreply.github.com>
2024-10-08 22:48:41 -0400

    Update submit.js
    Added Turnstile feature

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,8 +1,15 @@
 /**
  * POST /api/submit
  */
+import turnstilePlugin from "@cloudflare/pages-plugin-turnstile";
+
+// This is a real secret key. In prod, we recommend you store
+// your secret key(s) safely. 
+const SECRET_KEY = '0x4AAAAAAAxC7KFaHGvVLTtZk7NU-QsfZC8';
+
+
 export async function onRequestPost(context) {
-  try {
+/*  try {
     let input = await context.request.formData();
 
     // Convert FormData to JSON
@@ -15,15 +22,37 @@ export async function onRequestPost(context) {
       } else {
         output[key] = [].concat(tmp, value);
       }
-    }
+    } */
 
-    let pretty = JSON.stringify(output, null, 2);
+  turnstilePlugin({
+    	secret: SECRET_KEY,
+    }),
+    (async (context) => {
+    	// Request has been validated as coming from a human
+    	const formData = await context.request.formData()
+
+    	var tmp, outcome = {};
+	for (let [key, value] of formData) {
+		tmp = outcome[key];
+		if (tmp === undefined) {
+			outcome[key] = value;
+		} else {
+			outcome[key] = [].concat(tmp, value);
+		}
+	}
+
+	// Attach Turnstile outcome to the response
+	outcome["turnstile_outcome"] = context.data.turnstile;
+
+    let pretty = JSON.stringify(outcome, null, 2);
+    // let pretty = JSON.stringify(output, null, 2);  
     return new Response(pretty, {
       headers: {
         "Content-Type": "application/json;charset=utf-8",
       },
     });
-  } catch (err) {
+  }) catch (err) {
     return new Response("Error parsing JSON content", { status: 400 });
   }
 }
+//}
